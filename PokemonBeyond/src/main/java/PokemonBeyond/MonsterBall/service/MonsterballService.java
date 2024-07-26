655b6fed92aaf8dfdee563f05b3961ed7d722cd3
Vorschlag-bit <bustout@naver.com>
2024-07-26 20:10:40 +0900

    일단 제 브랜치에다가 푸쉬를 해보겠습니다

---
+++ PokemonBeyond/src/main/java/PokemonBeyond/MonsterBall/service/MonsterballService.java
@@ -0,0 +1,13 @@
+package PokemonBeyond.MonsterBall.service;
+
+import PokemonBeyond.MonsterBall.repository.MonsterBallRepository;
+
+public class MonsterballService {
+
+    private final MonsterBallRepository monsterBallRepository = new MonsterBallRepository();
+
+    public MonsterballService() {
+    }
+
+
+}
