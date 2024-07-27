80765cf48c2ac7f96fc563abdd914e5ed8c33d05
Vorschlag-bit <bustout@naver.com>
2024-07-27 22:39:13 +0900

    commit

--- PokemonBeyond/src/main/java/PokemonBeyond/MonsterBall/aggregate/MyPokemon.java
+++ PokemonBeyond/src/main/java/PokemonBeyond/MonsterBall/aggregate/MyPokemon.java
@@ -3,26 +3,7 @@ package PokemonBeyond.MonsterBall.aggregate;
 import java.util.ArrayList;
 
 public class MyPokemon {
-
-    private ArrayList<MonsterBall> ownPokemons = new ArrayList<MonsterBall>();
-    private int MemberId;
-
-    public MyPokemon() {
-    }
-
-    public int getMemberId() {
-        return MemberId;
-    }
-
-    public void setMemberId(int memberId) {
-        MemberId = memberId;
-    }
-
-    public ArrayList<MonsterBall> getOwnPokemons() {
-        return ownPokemons;
-    }
-
-    public void setOwnPokemons(ArrayList<MonsterBall> ownPokemons) {
-        this.ownPokemons = ownPokemons;
-    }
+    private MonsterBall monsterBall;
+    private Pokemon pokemon;
+    private String name;
 }
--- PokemonBeyond/src/main/java/PokemonBeyond/run/Application.java
+++ PokemonBeyond/src/main/java/PokemonBeyond/run/Application.java
@@ -1,24 +1,9 @@
 package PokemonBeyond.run;
 
-import PokemonBeyond.MonsterBall.service.MonsterballService;
-
 import java.util.Scanner;
 
 public class Application {
-    private static final MonsterballService monsterballService = new MonsterballService();
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        System.out.println("몬스터볼 시스템 Start");
-        System.out.println("1. 갖고있는 몬스터볼 보기");
-        System.out.println("2. 보유 몬스터 오박사님께 보내기");
-        System.out.println("수행하실 기능을 눌러주세요: ");
-
-        int choice = sc.nextInt();
-
-        switch (choice) {
-            case 1:
-                monsterballService.showMyPokemons();
-        }
     }
 }
