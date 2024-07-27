967f23f7b05747c193127258ef729f824b106d3c
Vorschlag-bit <bustout@naver.com>
2024-07-27 22:39:13 +0900

    commit

--- PokemonBeyond/src/main/java/PokemonBeyond/MonsterBall/aggregate/MonsterBall.java
+++ PokemonBeyond/src/main/java/PokemonBeyond/MonsterBall/aggregate/MonsterBall.java
@@ -1,29 +1,10 @@
 package PokemonBeyond.MonsterBall.aggregate;
 
 public class MonsterBall {
-    private int probability = 100;
-    private String name = "몬스터볼";
-
-    public void setPokemon(Pokemon pokemon) {
-        this.pokemon = pokemon;
-    }
-
-    public Pokemon getPokemon() {
-        return pokemon;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getProbability() {
-        return probability;
-    }
-
-    private Pokemon pokemon;
+    // 우선 볼 종류가 하나뿐이므로 몬스터볼, 잡는 확률은 100으로 고정
+   private String Ballname = "몬스터볼";
+   private int Probability = 100;
 
     public MonsterBall() {
     }
-
-
 }
