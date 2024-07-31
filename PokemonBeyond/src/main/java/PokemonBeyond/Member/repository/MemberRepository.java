f65e11e5de4c983d83fcf91b03ee31a29f21e3f4
Haegwan-Choe <kkhagwan@naver.com>
2024-07-31 19:55:00 +0900

    feature : 회원 기능 구현
    회원 수정, 삭제, 닉네임으로 찾기까지 구현

--- PokemonBeyond/src/main/java/PokemonBeyond/Member/repository/MemberRepository.java
+++ PokemonBeyond/src/main/java/PokemonBeyond/Member/repository/MemberRepository.java
@@ -5,34 +5,83 @@ import PokemonBeyond.Member.stream.MyObjectOutput;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.List;
 
 public class MemberRepository {
 
-    String filePath = "src/main/java/PokemonBeyond/Member/db/memberDB.dat";
-    File file = new File(filePath);
-
-    private ArrayList<Member> memberList = new ArrayList<>();
+    private final String filePath = "src\\main/java/PokemonBeyond/Member/db/memberDB.dat";
+    private final File file;
+    private final ArrayList<Member> memberList = new ArrayList<>();
 
     public MemberRepository() {
 
+        file = new File(filePath);
+        // 파일이 없는 경우
         if (!file.exists()) {
-            try {
-                file.createNewFile();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-
+            // 파일 생성 및 헤더 추가
+            saveMembers(file, memberList);
         }
-
-
+        loadMember(file);
 
     }
 
+    private void loadMember(File file) {
+        // 파일을 읽어 ArrayList에 저장
+        ObjectInputStream ois = null;
+        try {
+            ois = new ObjectInputStream(
+                    new BufferedInputStream(
+                            new FileInputStream(file)
+                    )
+            );
 
-    public int insertMember(Member newMember) {
+            while(true) {
+                memberList.add((Member)ois.readObject());
+            }
+        } catch (EOFException e) {
+            System.out.println("");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                if(ois != null) ois.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
 
+    }
+    private void saveMembers(File file, ArrayList<Member> memberList) {
+        // 파일을 덮어 씌워 저장
+        ObjectOutputStream oos = null;
+        try {
+            oos = new ObjectOutputStream(
+                    new BufferedOutputStream(
+                            new FileOutputStream(file)
+                    )
+            );
+
+            /* 설명. 초기 회원 세명을 각각 객체 출력 내보내기 */
+            for(Member member: memberList) {
+                oos.writeObject(member);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                if(oos != null) oos.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public boolean insertMember(Member newMember) {
+        // 파일에 이어붙여 저장
         MyObjectOutput moo = null;
-        int result = 0;
+        boolean result = false;
         try {
             moo = new MyObjectOutput(
                     new BufferedOutputStream(
@@ -41,12 +90,14 @@ public class MemberRepository {
             );
 
             /* 궁금. 왜 그냥 newMember를 넣으면 안되고 하나씩 넣어줘야하지??? */
-            moo.writeObject(newMember.getId() + newMember.getPwd() + newMember.getName()
-            + newMember.getAge() + newMember.getNickName());
+//            moo.writeObject(newMember.getId() + newMember.getPwd() + newMember.getName()
+//            + newMember.getAge() + newMember.getNickName());
+
+            moo.writeObject(newMember);
 
             memberList.add(newMember);
 
-            result = 1;
+            result = true;
         } catch (IOException e) {
             throw new RuntimeException(e);
         } finally {
@@ -56,11 +107,54 @@ public class MemberRepository {
                 throw new RuntimeException(e);
             }
         }
-
         return result;
     }
 
+    public Member findMemberId(String id) {
+        for (Member member : memberList) {
+            if (member.getId().equals(id)) {
+                return member;
+            }
+        }
+        return null;
+    }
+
+    public Member findMemberNickname(String nickName) {
+        for (Member member : memberList) {
+            if (member.getNickName().equals(nickName)) {
+                return member;
+            }
+        }
+        return null;
+    }
+
+    public void updateMember(Member member) {
+        Member Member = findMemberId(member.getId());
+
+        Member.setPwd(member.getPwd());
+        Member.setName(member.getName());
+        Member.setAge(member.getAge());
+        Member.setNickName(member.getNickName());
+        saveMembers(file, memberList);
+    }
 
 
+    public Member logInCheckMember(Member logInMember) {
+        for (Member member : memberList) {
+            if (member.getId().equals(logInMember.getId())) {
+                if (member.getPwd().equals(logInMember.getPwd())) {
+                    return member;
+                }
+            }
+        }
+        return null;
+    }
 
+    public void removeMember(Member member) {
+        memberList.remove(member);
+        System.out.println("포켓몬 세계는 언제나 당신을 기다립니다.");
+
+        saveMembers(file, memberList);
+
+    }
 }
--- PokemonBeyond/src/main/java/PokemonBeyond/Member/service/MemberService.java
+++ PokemonBeyond/src/main/java/PokemonBeyond/Member/service/MemberService.java
@@ -5,17 +5,48 @@ import PokemonBeyond.Member.repository.MemberRepository;
 
 public class MemberService {
 
-    private final MemberRepository mr = new MemberRepository();
+    private MemberRepository mr = new MemberRepository();
 
     public MemberService() {
     }
 
     public void registMember(Member newMember) {
 
-        int result = mr.insertMember(newMember);
+        boolean result = mr.insertMember(newMember);
 
-        if (result == 1) {
+        if (result == true) {
             System.out.println(newMember.getNickName() + "님 포켓몬 세계에 오신걸 환영합니다!!!");
         }
+
+    }
+
+    public Member logInMember(Member logInMember) {
+
+        // 로그인 전 확인
+        Member checkMember = mr.logInCheckMember(logInMember);
+
+        if (checkMember != null) {
+            return checkMember;
+        } else if (checkMember == null) {
+            System.out.println("아이디 및 패스워드가 틀렸거나 없는 아이디입니다.");
+        }
+        return null;
+    }
+
+    public Member searchMember(String nickName) {
+        Member member = mr.findMemberNickname(nickName);
+        if (member == null) {
+            System.out.println("일치하는 회원이 없습니다.");
+        }
+        return member;
+    }
+
+    public void modifyMember(Member member) {
+        mr.updateMember(member);
+    }
+
+    public void exitMember(Member member) {
+        mr.removeMember(member);
+
     }
 }
